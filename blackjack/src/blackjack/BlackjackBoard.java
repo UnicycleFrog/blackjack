c117ba66b1a0dc99011db68c2556ede67c696554
cable74 <LeeH4309@sduhsd.lan>
2016-05-05 10:48:50 -0700

    dank code

--- blackjack/src/blackjack/BlackjackBoard.java
+++ blackjack/src/blackjack/BlackjackBoard.java
@@ -3,10 +3,35 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
- * The ElevensBoard class represents the board in a game of Elevens.
+ * Black jack dumb code
+ * 
+ * int player count = sum of player cards
+ * int dealer count = sum of dealer cards
+ * 
+ * deal cards.....
+ * if player or dealer count ==21, player or dealer win
+ * 
+ * if player count<21 player have option to hit or stand                  loop it a few times
+ * if hit, player count = new random card + player count
+ * 
+ * else (player over 21/bust) player loose
+ * 
+ * 
+ * 
+ * if player stand 
+ * if dealer count< 17, dealer hit. else dealer stand
+ * if dealer > 21, dealer bust
+ * 
+ * 
+ * if player>dealer, player win. else dealer win.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  */
 public class BlackjackBoard extends Board {
-
 	/**
 	 * The size (number of cards) on the board.
 	 */
@@ -54,121 +79,13 @@ public class BlackjackBoard extends Board {
 		super(BOARD_SIZE, RANKS, SUITS, POINT_VALUES);
 	}
 
-/*	*//**
-	 * Start a new game by shuffling the deck and
-	 * dealing some cards to this board.
-	 *//*
-	public void newGame() {
-		deck.shuffle();
-		dealMyCards();
+	public int score(List<Integer> selectedCards) {
+		int player = 0;
+		int dealer = 0;
+		for (int i : selectedCards)
+			player += i;
+		return 0;
 	}
-
-	*//**
-	 * Accesses the size of the board.
-	 * Note that this is not the number of cards it contains,
-	 * which will be smaller near the end of a winning game.
-	 * @return the size of the board
-	 *//*
-	public int size() {
-		return cards.length;
-	}
-
-	*//**
-	 * Determines if the board is empty (has no cards).
-	 * @return true if this board is empty; false otherwise.
-	 *//*
-	public boolean isEmpty() {
-		for (int k = 0; k < cards.length; k++) {
-			if (cards[k] != null) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	*//**
-	 * Deal a card to the kth position in this board.
-	 * If the deck is empty, the kth card is set to null.
-	 * @param k the index of the card to be dealt.
-	 *//*
-	public void deal(int k) {
-		cards[k] = deck.deal();
-	}
-
-	*//**
-	 * Accesses the deck's size.
-	 * @return the number of undealt cards left in the deck.
-	 *//*
-	public int deckSize() {
-		return deck.size();
-	}
-
-	*//**
-	 * Accesses a card on the board.
-	 * @return the card at position k on the board.
-	 * @param k is the board position of the card to return.
-	 *//*
-	public Card cardAt(int k) {
-		return cards[k];
-	}
-
-	*//**
-	 * Replaces selected cards on the board by dealing new cards.
-	 * @param selectedCards is a list of the indices of the
-	 *        cards to be replaced.
-	 *//*
-	public void replaceSelectedCards(List<Integer> selectedCards) {
-		for (Integer k : selectedCards) {
-			deal(k.intValue());
-		}
-	}
-
-	*//**
-	 * Gets the indexes of the actual (non-null) cards on the board.
-	 *
-	 * @return a List that contains the locations (indexes)
-	 *         of the non-null entries on the board.
-	 *//*
-	public List<Integer> cardIndexes() {
-		List<Integer> selected = new ArrayList<Integer>();
-		for (int k = 0; k < cards.length; k++) {
-			if (cards[k] != null) {
-				selected.add(new Integer(k));
-			}
-		}
-		return selected;
-	}
-
-	*//**
-	 * Generates and returns a string representation of this board.
-	 * @return the string version of this board.
-	 *//*
-	public String toString() {
-		String s = "";
-		for (int k = 0; k < cards.length; k++) {
-			s = s + k + ": " + cards[k] + "\n";
-		}
-		return s;
-	}
-
-	*//**
-	 * Determine whether or not the game has been won,
-	 * i.e. neither the board nor the deck has any more cards.
-	 * @return true when the current game has been won;
-	 *         false otherwise.
-	 *//*
-	public boolean gameIsWon() {
-		if (deck.isEmpty()) {
-			for (Card c : cards) {
-				if (c != null) {
-					return false;
-				}
-			}
-			return true;
-		}
-		return false;
-	}*/
-
 	/**
 	 * Determines if the selected cards form a valid group for removal.
 	 * In Elevens, the legal groups are (1) a pair of non-face cards
@@ -196,16 +113,6 @@ public class BlackjackBoard extends Board {
 		return(containsPairSum11(cardIndexes()) || containsJQK(cardIndexes()));
 	}
 
-
-	/**
-	 * Deal cards to this board to start the game.
-	 */
-/*	private void dealMyCards() {
-		for (int k = 0; k < cards.length; k++) {
-			cards[k] = deck.deal();
-		}
-	}
-*/
 	/**
 	 * Check for an 11-pair in the selected cards.
 	 * @param selectedCards selects a subset of this board.  It is list
