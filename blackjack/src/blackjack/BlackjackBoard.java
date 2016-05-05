ed62c1c133f7579a1f3e00fd9a286e6b1033f4c7
UnicycleFrog <c4ba4105@opayq.com>
2016-05-05 10:59:55 -0700

    edit a bit of the bjb

--- blackjack/src/blackjack/BlackjackBoard.java
+++ blackjack/src/blackjack/BlackjackBoard.java
@@ -28,7 +28,7 @@ public class BlackjackBoard extends Board {
 	 * The values of the cards for this game to be sent to the deck.
 	 */
 	private static final int[] POINT_VALUES =
-		{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
+		{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
 
 
 	/**
@@ -53,121 +53,6 @@ public class BlackjackBoard extends Board {
 	public BlackjackBoard() {
 		super(BOARD_SIZE, RANKS, SUITS, POINT_VALUES);
 	}
-
-/*	*//**
-	 * Start a new game by shuffling the deck and
-	 * dealing some cards to this board.
-	 *//*
-	public void newGame() {
-		deck.shuffle();
-		dealMyCards();
-	}
-
-	*//**
-	 * Accesses the size of the board.
-	 * Note that this is not the number of cards it contains,
-	 * which will be smaller near the end of a winning game.
-	 * @return the size of the board
-	 *//*
-	public int size() {
-		return cards.length;
-	}
-
-	*//**
-	 * Determines if the board is empty (has no cards).
-	 * @return true if this board is empty; false otherwise.
-	 *//*
-	public boolean isEmpty() {
-		for (int k = 0; k < cards.length; k++) {
-			if (cards[k] != null) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	*//**
-	 * Deal a card to the kth position in this board.
-	 * If the deck is empty, the kth card is set to null.
-	 * @param k the index of the card to be dealt.
-	 *//*
-	public void deal(int k) {
-		cards[k] = deck.deal();
-	}
-
-	*//**
-	 * Accesses the deck's size.
-	 * @return the number of undealt cards left in the deck.
-	 *//*
-	public int deckSize() {
-		return deck.size();
-	}
-
-	*//**
-	 * Accesses a card on the board.
-	 * @return the card at position k on the board.
-	 * @param k is the board position of the card to return.
-	 *//*
-	public Card cardAt(int k) {
-		return cards[k];
-	}
-
-	*//**
-	 * Replaces selected cards on the board by dealing new cards.
-	 * @param selectedCards is a list of the indices of the
-	 *        cards to be replaced.
-	 *//*
-	public void replaceSelectedCards(List<Integer> selectedCards) {
-		for (Integer k : selectedCards) {
-			deal(k.intValue());
-		}
-	}
-
-	*//**
-	 * Gets the indexes of the actual (non-null) cards on the board.
-	 *
-	 * @return a List that contains the locations (indexes)
-	 *         of the non-null entries on the board.
-	 *//*
-	public List<Integer> cardIndexes() {
-		List<Integer> selected = new ArrayList<Integer>();
-		for (int k = 0; k < cards.length; k++) {
-			if (cards[k] != null) {
-				selected.add(new Integer(k));
-			}
-		}
-		return selected;
-	}
-
-	*//**
-	 * Generates and returns a string representation of this board.
-	 * @return the string version of this board.
-	 *//*
-	public String toString() {
-		String s = "";
-		for (int k = 0; k < cards.length; k++) {
-			s = s + k + ": " + cards[k] + "\n";
-		}
-		return s;
-	}
-
-	*//**
-	 * Determine whether or not the game has been won,
-	 * i.e. neither the board nor the deck has any more cards.
-	 * @return true when the current game has been won;
-	 *         false otherwise.
-	 *//*
-	public boolean gameIsWon() {
-		if (deck.isEmpty()) {
-			for (Card c : cards) {
-				if (c != null) {
-					return false;
-				}
-			}
-			return true;
-		}
-		return false;
-	}*/
 
 	/**
 	 * Determines if the selected cards form a valid group for removal.
@@ -196,16 +81,6 @@ public class BlackjackBoard extends Board {
 		return(containsPairSum11(cardIndexes()) || containsJQK(cardIndexes()));
 	}
 
-
-	/**
-	 * Deal cards to this board to start the game.
-	 */
-/*	private void dealMyCards() {
-		for (int k = 0; k < cards.length; k++) {
-			cards[k] = deck.deal();
-		}
-	}
-*/
 	/**
 	 * Check for an 11-pair in the selected cards.
 	 * @param selectedCards selects a subset of this board.  It is list
